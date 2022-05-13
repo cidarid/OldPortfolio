35e41112a232950218f1df010a99bb0bfa49fa22
locelus <spfletcher2003@gmail.com>
2022-05-12 20:57:29 -0400

    Centered quote elements

--- components/Quote.js
+++ components/Quote.js
@@ -1,5 +1,5 @@
 export default function Quote({ text, quotee, quoteeRole }) {
-  return <div>
+  return <div class="quote">
     <h2>"{text ? text : 'Default text'}"</h2>
     <p>
       <strong>{quotee ? quotee : 'Default quotee'}</strong>
