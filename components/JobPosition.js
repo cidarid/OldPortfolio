4d53bcbcbffc49dd934c4f9069fb7f5e03afe07b
locelus <spfletcher2003@gmail.com>
2023-03-22 15:41:28 -0700

    Removing contact info, adding blocks

--- components/JobPosition.js
+++ components/JobPosition.js
@@ -1,7 +1,7 @@
-export default function JobHeader({title, employer, location, contact, dates, tasks}) {
+export default function JobHeader({title, employer, location, dates, tasks}) {
   return <>
     <p style={{textAlign: "left"}}><strong>{title + (employer ? ' | ' + employer : null)}</strong> – {location}
-      {contact ? ", " + contact : null}<span style={{float: "right"}}>{dates}</span></p>
+      <span style={{float: "right"}}>{dates}</span></p>
     <ul>
       {tasks ? tasks.map(task => (<li>{task}</li>)) : null}
     </ul>
